5a31629a91f4bb54df8a7e322ff9131b8f808b8c
AntonioAlejandro01 <antonioalejandroserranoramirez@gmail.com>
2021-01-03 23:16:26 +0100

    add more test

--- src/test/java/com/antonioalejandro/smkt/oauth/clients/security/test/InfoAdicionalTokenTest.java
+++ src/test/java/com/antonioalejandro/smkt/oauth/clients/security/test/InfoAdicionalTokenTest.java
@@ -29,19 +29,20 @@ class InfoAdicionalTokenTest {
 	private InfoAdicionalToken infoAdicionalToken;
 	@Mock
 	private OauthService service;
-	
+
 	@Mock
 	private OAuth2Authentication authentication;
-	
+
 	@Mock
-	private DefaultOAuth2AccessToken accessToken; 
-	
+	private DefaultOAuth2AccessToken accessToken;
+
 	private static final String NAME = "name";
-	
+
 	@BeforeEach
 	public void init() {
 		MockitoAnnotations.initMocks(this);
 	}
+
 	@Test
 	void test() throws Exception {
 		UserResponse response = new UserResponse();
@@ -65,6 +66,58 @@ class InfoAdicionalTokenTest {
 		List<String> scopes = new ArrayList<String>(1);
 		scopes.add(UtilsForTesting.DATA_OK);
 		scopeResponse.setScopes(scopes);
+		when(service.findScopesByRoleId(0L)).thenReturn(scopeResponse);
+
+		OAuth2AccessToken token = infoAdicionalToken.enhance(accessToken, authentication);
+
+		assertNotNull(token);
+
+	}
+
+	@Test
+	void testEmpty() throws Exception {
+		UserResponse response = new UserResponse();
+		User user = response.new User();
+		user.setEmail(UtilsForTesting.DATA_OK);
+		user.setLastname(UtilsForTesting.DATA_OK);
+		user.setName(UtilsForTesting.DATA_OK);
+		user.setPassword(UtilsForTesting.DATA_OK);
+		user.setRole(UtilsForTesting.DATA_OK);
+		user.setUsername(UtilsForTesting.DATA_OK);
+		response.setUser(user);
+		when(authentication.getName()).thenReturn(NAME);
+		when(service.findUserByUsername(authentication.getName())).thenReturn(response);
+		RoleResponse roleResponse = new RoleResponse();
+		roleResponse.setRole(null);
+		when(service.findRoleByName(UtilsForTesting.DATA_OK)).thenReturn(roleResponse);
+
+		OAuth2AccessToken token = infoAdicionalToken.enhance(accessToken, authentication);
+
+		assertNotNull(token);
+
+	}
+	@Test
+	void testEmptyScopes() throws Exception {
+		UserResponse response = new UserResponse();
+		User user = response.new User();
+		user.setEmail(UtilsForTesting.DATA_OK);
+		user.setLastname(UtilsForTesting.DATA_OK);
+		user.setName(UtilsForTesting.DATA_OK);
+		user.setPassword(UtilsForTesting.DATA_OK);
+		user.setRole(UtilsForTesting.DATA_OK);
+		user.setUsername(UtilsForTesting.DATA_OK);
+		response.setUser(user);
+		when(authentication.getName()).thenReturn(NAME);
+		when(service.findUserByUsername(authentication.getName())).thenReturn(response);
+		RoleResponse roleResponse = new RoleResponse();
+		Role role = roleResponse.new Role();
+		role.setId(0L);
+		role.setName(UtilsForTesting.DATA_OK);
+		roleResponse.setRole(role);
+		when(service.findRoleByName(role.getName())).thenReturn(roleResponse);
+		ScopeResponse scopeResponse = new ScopeResponse();
+		
+		scopeResponse.setScopes(List.of());
 		when(service.findScopesByRoleId(0L)).thenReturn(scopeResponse);
 		
 		OAuth2AccessToken token =  infoAdicionalToken.enhance(accessToken, authentication);
