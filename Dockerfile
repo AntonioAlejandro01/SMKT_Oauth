985825388462314c24af0d2eed1264c725887d5c
AntonioAlejandro01 <antonioalejandroserranoramirez@gmail.com>
2021-01-04 21:14:51 +0100

    update dockerfile and fix bug

--- Dockerfile
+++ Dockerfile
@@ -14,13 +14,16 @@ WORKDIR /opt/server
 
 COPY --from=build /app.jar  ./app.jar
 
+ARG smkt_users=smkt-users
 ARG port=9100
 ARG eureka_url=http://smkt-eureka:8761/eureka
+ARG app_secret=Sm@artKitchen
 
+ENV SMKT_USERS ${smkt_users}
+ENV APP_SECRET ${app_secret} 
 ENV PORT ${port}
 ENV EUREKA_URL ${eureka_url}
-
 EXPOSE ${PORT}
 
-CMD java -jar app.jar --server.port="${PORT}" --eureka.client.service-url.defaultZone="${EUREKA_URL}"
+CMD java -jar app.jar --server.port="${PORT}" --eureka.client.service-url.defaultZone="${EUREKA_URL}" --usersId="${SMKT_USERS}" --app-secret="${APP_SECRET}"
 
--- src/main/java/com/antonioalejandro/smkt/oauth/services/OauthService.java
+++ src/main/java/com/antonioalejandro/smkt/oauth/services/OauthService.java
@@ -12,7 +12,6 @@ import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.http.HttpEntity;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -51,27 +50,17 @@ public class OauthService implements IOauthService, UserDetailsService {
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-		try {
-			final UserResponse userResponse = findUserByUsername(username);
-			if (userResponse.getUser() == null) {
-				throw new NullPointerException(userResponse.getMessage());
-			}
-			final UserResponse.User user = userResponse.getUser();
-			final GrantedAuthority authority = new SimpleGrantedAuthority(user.getRole());
-			log.info("Usuario autenticado: " + username);
-			log.info("ID: " + user.getId() + ", USERNAME: " + user.getUsername() + ", EMAIL: " + user.getEmail()
-					+ "Role: " + authority.getAuthority());
-
-			return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), true,
-					true, true, true, Arrays.asList(authority));
-
-		} catch (final Exception e) {
-			log.error(e.getMessage());
-			log.error("Error en el login, no existe el usuario '" + username + "' en el sistema");
-			throw new UsernameNotFoundException(
-					"Error en el login, no existe el usuario '" + username + "' en el sistema");
+		final UserResponse userResponse = findUserByUsername(username);
+		if (userResponse.getUser() == null) {
+			throw new UsernameNotFoundException(String.format("User %s don't exists", username));
 		}
+		final UserResponse.User user = userResponse.getUser();
+		final GrantedAuthority authority = new SimpleGrantedAuthority(user.getRole());
+		log.info("Usuario {} autenticado", user.getUsername());
+		log.info("User: {}", user);
 
+		return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), true,
+				true, true, true, Arrays.asList(authority));
 	}
 
 	/**
@@ -84,10 +73,8 @@ public class OauthService implements IOauthService, UserDetailsService {
 	public UserResponse findUserByUsername(String username) {
 		WebClient client = WebClientFactory
 				.getWebClient(WebClientFactory.getURLInstanceService(usersId, discoveryClient), true);
-		HttpEntity<UserResponse> entity = client.get()
-				.uri(String.format("/users/search?filter=username&value=%s", username)).retrieve()
-				.toEntity(UserResponse.class).block();
-		return entity.getBody();
+		return client.get().uri(String.format("/users/search?filter=username&value=%s", username)).retrieve()
+				.bodyToMono(UserResponse.class).block();
 	}
 
 	/**
